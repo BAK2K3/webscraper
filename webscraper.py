aef18b12eee4e91136849d1e9ebc7263576a1b96
bak2k3 <benjamin.a.kavanagh@gmail.com>
2021-06-11 17:45:58 +0100

    Add error checking and update docstrings.

--- webscraper.py
+++ webscraper.py
@@ -14,15 +14,21 @@ Created by Benjamin Kavanagh (BAK2K3) 2021.
 
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 # starting URL
-starting_url = "https://www.vk.com/"
+starting_url = "www.vk.com"
 
 
 def webpage_to_soup(url):
     """
     Convert a URL to a Soup Object
+
+    Attributes
+    ----------
+    url : str
+        The url to convert into a Soup object
     """
     webpage = requests.get(url)
     soup_object = BeautifulSoup(webpage.content, features='html.parser')
@@ -31,27 +37,51 @@ def webpage_to_soup(url):
 
 def scrape_url(url, domain=None, visited_webpages={*()}, iteration=0):
     """
-    - Identify all A tags on page
-    - Print all tags
-    - Find all domain tags
-    - Check if next domain tag is in set
-    - If not, add it, and call self with new tag    
+    - Identifies/Prints all <A> tag Hrefs on a given page
+    - Finds all internal domain <A> tag Hrefs
+    - Check if domain Href has been visited previously
+    - If not, add it to set, and recursively call with current href
+
+    Attributes
+    ----------
+    url : str
+        The URL to be scraped
+    domain: str
+        The domain of the URL (Default=None)
+    visitied_webpages: set
+        Set of unique web addresses visited (Default = Empty Set)
+    iteration: int 
+        Function's current iteration (Default = 0)
     """
 
-    print(iteration)
+    # Check that a valid iteration has been provided
+    if not isinstance(iteration, int):
+        raise ValueError("Instance variable must be an integer.")
 
-    # If first iteration of loop, initialise set and domain
+    # If first iteration of loop initialise domain
     if iteration == 0:
+
+        # Prepares URL provided if required
+        if "http" not in url:
+            url = "https://" + url
+        # Check that a valid URL has been provided
+        if not urlparse(url).netloc:
+            raise ValueError("Invalid URL provided.")
+        # Check that visited_webpages is a set
+        if not isinstance(visited_webpages, set):
+            raise TypeError("visited_webpages must be a set")
+
         # Re-define specified url for page specific definition
         domain = requests.get(url).request.url
+        print(f"Starting URL: {domain}")
 
     # Create a new soup object from the current url
     new_webpage = webpage_to_soup(url)
 
-    # Obtain all "a" links on page
+    # Obtain all <a> links on page
     all_links = new_webpage.findAll("a", href=True)
 
-    print(f"Current Page: {url}")
+    # Print all links on current page
     print("All links on current page:-")
     for link in all_links:
         print(f"\t{link['href']}")
@@ -78,21 +108,16 @@ def scrape_url(url, domain=None, visited_webpages={*()}, iteration=0):
 
                 # Check to see whether the page has already been visited
                 if raw_link not in visited_webpages:
+
                     # Increment iteration
                     iteration += 1
                     # Append website to set
                     visited_webpages.add(raw_link)
-
                     print(f"Visiting Unique URL: {raw_link}")
-
-                    # Call function again
+                    # Recursive function call
                     scrape_url(raw_link, domain, visited_webpages, iteration)
 
-                # else:
-                    # Confirm URL already visited
-                    # DEBUG
-                    # print("URL already visited!")
-
+    # Return set of unique internal domain refs
     return visited_webpages
 
 
